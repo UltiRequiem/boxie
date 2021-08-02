9f599003d567cff7dba94a9cf5ecc6e28dd85383
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-02 12:30:30 -0500

    :art: Update

---
+++ boxie/__main__.py
@@ -0,0 +1,4 @@
+from .core import run
+
+if __name__ == '__main__':
+    run()
--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from distutils.core import setup
 from boxie.version import __version__
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name="boxie",
     version=__version__,
     description="Like Figlet but puts the text in a box",
+    long_description=README,
+    long_description_content_type="text/markdown",
     author="UltiRequiem",
     author_email="eliaz.bobadilladev@gmail.com",
     url="https://github.com/UltiRequiem/boxie",
