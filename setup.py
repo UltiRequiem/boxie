0427a39057bfa89d632b82f8099f5be5e3756168
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-02 13:41:58 -0500

    :art: DOCS

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     author="UltiRequiem",
     author_email="eliaz.bobadilladev@gmail.com",
     url="https://github.com/UltiRequiem/boxie",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     include_package_data=True,
     license="MIT",
     scripts=["./bin/boxie"],
