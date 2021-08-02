cd8e49a068bd37fc6de913d62ef149603f22d8e1
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-02 13:11:08 -0500

    :bookmark: Version 1.0.4

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from boxie.version import __version__
 setup(
     name="boxie",
     version=__version__,
-    description="Like Figlet but puts the text in a box",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
     author="UltiRequiem",
     author_email="eliaz.bobadilladev@gmail.com",
     url="https://github.com/UltiRequiem/boxie",
