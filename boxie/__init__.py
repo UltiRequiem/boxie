0c13edfb45788a0626ab1dd0773702aa7540d03c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-02 13:30:01 -0500

    :art: Update

--- boxie/__init__.py
+++ boxie/__init__.py
@@ -1,4 +1,4 @@
-
 """
 This file tells Python that this is a submodule
 """
+from .core import *
---
+++ test.py
@@ -0,0 +1,9 @@
+from boxie import box_borders
+
+
+@box_borders
+def print_console(txt):
+    print(txt)
+
+
+print_console("Hello World")
